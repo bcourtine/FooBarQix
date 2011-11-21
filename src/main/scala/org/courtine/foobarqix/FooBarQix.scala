2675ba3585eda1e346602162a997c9c4b571ab1f
Benoît Courtine <benoit.courtine@gmail.com>
2011-11-21 21:31:38 +0100

    Implémentation minimale d'une solution au problème (sans factorisation de code).

--- src/main/scala/org/courtine/foobarqix/FooBarQix.scala
+++ src/main/scala/org/courtine/foobarqix/FooBarQix.scala
@@ -10,6 +10,36 @@ package org.courtine.foobarqix
 object FooBarQix {
 
   def process(n: Int): String = {
-    return ""
+    modulo3(n) + modulo5(n) + modulo7(n) + contains(n.toString.toList) match {
+      case "" => n.toString
+      case notEmpty => notEmpty
+    }
   }
+
+  def modulo3(n: Int): String =
+    n % 3 match {
+      case 0 => "Foo"
+      case _ => ""
+    }
+
+  def modulo5(n: Int): String =
+    n % 5 match {
+      case 0 => "Bar"
+      case _ => ""
+    }
+
+  def modulo7(n: Int): String =
+    n % 7 match {
+      case 0 => "Qix"
+      case _ => ""
+    }
+
+  def contains(chars: List[Char]): String =
+    chars match {
+      case Nil => ""
+      case '3' :: tail => "Foo" + contains(tail)
+      case '5' :: tail => "Bar" + contains(tail)
+      case '7' :: tail => "Qix" + contains(tail)
+      case _ :: tail => contains(tail)
+    }
 }
